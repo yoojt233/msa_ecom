1bc88b2a0bf42b8f198f632e75d9900b40509af0
유지태 <yoojt233@naver.com>
2024-10-18 16:00:20 +0900

    add: setting local repo's config path

---
+++ config-m-service/src/test/kotlin/service/configmservice/ConfigMServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package service.configmservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigMServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
